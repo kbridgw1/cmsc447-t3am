0e148630b1a13b69c37c099ae1140daa6f5bf358
kristinabridgwater <kbridgwa@vols.utk.edu>
2020-04-06 20:04:57 -0400

    testing changes

--- cmsc447app.py
+++ cmsc447app.py
@@ -1,21 +1,21 @@
-#imports
+# imports
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import SubmitField
 
-
+# configs
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecretkey"
 
 app.config["RECAPTCHA_PRIVATE_KEY"] = "6Lc6yOYUAAAAAPTvhl7pz5jt224kFXuxDibBIYtL"
 app.config["RECAPTCHA_PUBLIC_KEY"] = "6Lc6yOYUAAAAAKiCPcNN8bDw4cAqxJnwmn3WpHhw"
 
-
+# define form objects
 class Widgets(FlaskForm):
     recaptcha = RecaptchaField()
     submit = SubmitField(label="Submit")
 
-
+# define routes
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = Widgets()
@@ -32,5 +32,7 @@ def main():
 def terms():
     return render_template("cmsc447terms.html")
 
+
+# run in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
