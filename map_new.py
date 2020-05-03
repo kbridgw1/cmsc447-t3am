84fde36480f06e1098c79925c20f559c5db054f3
kristinabridgwater <kbridgwa@vols.utk.edu>
2020-05-03 14:49:27 -0400

    map working

--- map_new.py
+++ map_new.py
@@ -2,10 +2,10 @@ import folium
 import os
 
 def Map():
-    m = folium.Map(location=[39.045753,-76.641273], zoom_start=8, width='75%', height='75%')
+    m = folium.Map(location=[39.045753,-76.641273], zoom_start=8, width='65%', height='65%')
     cgeo = os.path.join('voterData/congressional_districts.json')
 
-    folium.GeoJson(cgeo, name='congressional').add_to(m)
+    folium.GeoJson(cgeo, name='precincts').add_to(m)
 
 
     m.save("templates/index.html")
