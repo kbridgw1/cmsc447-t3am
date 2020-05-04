8a67c4b12f982e0a31a1bfcde9921a2ed42080bb
kristinabridgwater <kbridgwa@vols.utk.edu>
2020-05-04 18:19:27 -0400

    WE HAVE PRECINCTS

--- map_new.py
+++ map_new.py
@@ -1,12 +1,24 @@
 import folium
 import os
+import pandas as pd
+import geopandas as gpd
+import branca
+import json
+import requests
+
+from folium.features import GeoJson, GeoJsonTooltip
 
 def Map():
     m = folium.Map(location=[39.045753,-76.641273], zoom_start=8, width='65%', height='65%')
-    cgeo = os.path.join('voterData/congressional_districts.json')
+    cgeo = os.path.join('originalgeo.json')
+    #districts = 'voterData/final_csv.csv'
+    #disctrict_data = pd.read_csv(districts)
+    #bins = list(dem_percent['dem'].quantile([0, 0.25, 0.5, 0.75, 1]))
 
-    folium.GeoJson(cgeo, name='precincts').add_to(m)
-
+    folium.GeoJson(
+        cgeo,
+        name='geojson'
+    ).add_to(m)
 
     m.save("templates/index.html")
     return m
